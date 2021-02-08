f838e6a7822b9cbea5e749707344e4227b49cc57
Matt <matt.spong@gmail.com>
2021-02-08 15:52:36 +1100

    Initial Commit

---
+++ Math App/Math_App.swift
@@ -0,0 +1,17 @@
+//
+//  Math_App.swift
+//  Math App
+//
+//  Created by Matthew Spong on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Math_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
